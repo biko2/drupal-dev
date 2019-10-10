0ca2e5c1044864327e9bf51eba3f5911f0d1011f
Juanjo López <juanjo.lopez@biko2.com>
2019-10-10 12:13:47 +0200

    Fixed pimpmylog and increased php memory_limit

--- docker/web/pimpmylog/config.user.php
+++ docker/web/pimpmylog/config.user.php
@@ -69,6 +69,79 @@
 	},
 
 	"files": {
-"FILES":"FILES"
+		"apache1": {
+			"display" : "Apache Error #1",
+			"path"    : "\/var\/log\/apache2\/error.log",
+			"refresh" : 5,
+			"max"     : 10,
+			"notify"  : true,
+			"format"  : {
+				"type"         : "HTTPD 2.4",
+				"regex"        : "|^\\[(.*) (.*) (.*) (.*):(.*):(.*)\\.(.*) (.*)\\] \\[(.*):(.*)\\] \\[pid (.*)\\] .*\\[client (.*):(.*)\\] (.*)(, referer: (.*))*$|U",
+				"export_title" : "Log",
+				"match"        : {
+					"Date"    : {
+						"M" : 2,
+						"d" : 3,
+						"H" : 4,
+						"i" : 5,
+						"s" : 6,
+						"Y" : 8
+					},
+					"IP"       : 12,
+					"Log"      : 14,
+					"Severity" : 10,
+					"Referer"  : 16
+				},
+				"types": {
+					"Date"     : "date:H:i:s",
+					"IP"       : "ip:http",
+					"Log"      : "preformatted",
+					"Severity" : "badge:severity",
+					"Referer"  : "link"
+				},
+				"exclude": {
+					"Log": ["\/PHP Stack trace:\/", "\/PHP *[0-9]*\\. \/"]
+				}
+			}
+		},
+		"apache2": {
+			"display" : "Apache Access #2",
+			"path"    : "\/var\/log\/apache2\/access.log",
+			"refresh" : 0,
+			"max"     : 10,
+			"notify"  : false,
+			"format"  : {
+				"type"         : "NCSA",
+				"regex"        : "|^((\\S*) )*(\\S*) (\\S*) (\\S*) \\[(.*)\\] \"(\\S*) (.*) (\\S*)\" ([0-9]*) (.*)( \"(.*)\" \"(.*)\"( [0-9]*/([0-9]*))*)*$|U",
+				"export_title" : "URL",
+				"match"        : {
+					"Date"    : 6,
+					"IP"      : 3,
+					"CMD"     : 7,
+					"URL"     : 8,
+					"Code"    : 10,
+					"Size"    : 11,
+					"Referer" : 13,
+					"UA"      : 14,
+					"User"    : 5,
+					"\u03bcs" : 16
+				},
+				"types": {
+					"Date"    : "date:H:i:s",
+					"IP"      : "ip:geo",
+					"URL"     : "txt",
+					"Code"    : "badge:http",
+					"Size"    : "numeral:0b",
+					"Referer" : "link",
+					"UA"      : "ua:{os.name} {os.version} | {browser.name} {browser.version}\/100",
+					"\u03bcs" : "numeral:0,0"
+				},
+				"exclude": {
+					"URL": ["\/favicon.ico\/", "\/\\.pml\\.php.*$\/"],
+					"CMD": ["\/OPTIONS\/"]
+				}
+			}
+		}
 	}
 }
